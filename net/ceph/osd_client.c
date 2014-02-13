bf9adbaff3e0a22bbb81d686ed405fb5d30bef7f
stephen hemminger <stephen@networkplumber.org>
2014-02-12 20:51:22 -0800

    net: remove unnecessary return's
    One of my pet coding style peeves is the practice of
    adding extra return; at the end of function.
    Kill several instances of this in network code.
    
    I suppose some coccinelle wizardy could do this automatically.
    
    Change-Id: I87241af11ae1b4f345ecedd8ee241a4c4a1c4b62
    Signed-off-by: Stephen Hemminger <stephen@networkplumber.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/ceph/osd_client.c
+++ net/ceph/osd_client.c
@@ -1873,7 +1873,6 @@ bad:
 	pr_err("osdc handle_map corrupt msg\n");
 	ceph_msg_dump(msg);
 	up_write(&osdc->map_sem);
-	return;
 }
 
 /*
@@ -2072,7 +2071,6 @@ done_err:
 
 bad:
 	pr_err("osdc handle_watch_notify corrupt msg\n");
-	return;
 }
 
 /*
