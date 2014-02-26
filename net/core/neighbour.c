30f719769c852e5785cb331941011013707c1b3c
Timo Teräs <timo.teras@iki.fi>
2014-02-26 11:43:04 +0200

    BACKPORT: neigh: probe application via netlink in NUD_PROBE
    iproute2 arpd seems to expect this as there's code and comments
    to handle netlink probes with NUD_PROBE set. It is used to flush
    the arpd cached mappings.
    
    opennhrp instead turns off unicast probes (so it can handle all
    neighbour discovery). Without this change it will not see NUD_PROBE
    probes and cannot reconfirm the mapping. Thus currently neigh entry
    will just fail and can cause few packets dropped until broadcast
    discovery is restarted.
    
    Earlier discussion on the subject:
    http://marc.info/?t=139305877100001&r=1&w=2
    
    Change-Id: I35664f8731fe319a2325c16a5243571e21aeb73d
    Signed-off-by: Timo Teräs <timo.teras@iki.fi>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    [syphyr: Backported to 3.10 and 3.4]
    Signed-off-by: L.W. Reek <syphyr@gmail.com>

--- net/core/neighbour.c
+++ net/core/neighbour.c
@@ -842,9 +842,10 @@ out:
 static __inline__ int neigh_max_probes(struct neighbour *n)
 {
 	struct neigh_parms *p = n->parms;
-	return (n->nud_state & NUD_PROBE) ?
-		p->ucast_probes :
-		p->ucast_probes + p->app_probes + p->mcast_probes;
+	int max_probes = p->ucast_probes + p->app_probes;
+	if (!(n->nud_state & NUD_PROBE))
+		max_probes += p->mcast_probes;
+	return max_probes;
 }
 
 static void neigh_invalidate(struct neighbour *neigh)
