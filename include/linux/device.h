c388b94cb06402d97ab7ec9565e109190a7c7859
Jean Delvare <jdelvare@suse.de>
2014-04-14 12:58:53 +0200

    driver core: Inline dev_set/get_drvdata
    dev_set_drvdata and dev_get_drvdata are now simple enough again that
    we can inline them as they used to be before commit b40284378.
    
    [apq8084: Due to the number of non-upstreamed devices, we don't
     dare to apply "driver core: dev_get_drvdata: Don't check for NULL dev"
    
     Therefore this patch inlines dev_get_drvdata with NULL dev check]
    
    Change-Id: Ic2de72c2bf9dfae78cff91dfe73acdf87866c442
    Signed-off-by: Jean Delvare <jdelvare@suse.de>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- include/linux/device.h
+++ include/linux/device.h
@@ -772,6 +772,18 @@ static inline void set_dev_node(struct device *dev, int node)
 }
 #endif
 
+static inline void *dev_get_drvdata(const struct device *dev)
+{
+	if (dev)
+		return dev->driver_data;
+	return NULL;
+}
+
+static inline void dev_set_drvdata(struct device *dev, void *data)
+{
+	dev->driver_data = data;
+}
+
 static inline struct pm_subsys_data *dev_to_psd(struct device *dev)
 {
 	return dev ? dev->power.subsys_data : NULL;
@@ -856,8 +868,6 @@ extern int device_move(struct device *dev, struct device *new_parent,
 extern const char *device_get_devnode(struct device *dev,
 				      umode_t *mode, kuid_t *uid, kgid_t *gid,
 				      const char **tmp);
-extern void *dev_get_drvdata(const struct device *dev);
-extern void dev_set_drvdata(struct device *dev, void *data);
 
 /*
  * Root device objects for grouping under /sys/devices
