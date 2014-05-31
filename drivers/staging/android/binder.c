2a0c6861ec802cfd1dcf1bf0ca34ebc4ee4e6d2f
Tair Rzayev <tair.rzayev@gmail.com>
2014-05-31 22:43:34 +0300

    staging: android: binder.c: Use more appropriate functions for euid retrieval
    Instead of getting the reference to whole credential structure, use
    task_euid() and current_euid() to get it.
    
    Change-Id: I83d83702769f938f47f6a998f589a720d7a9acb4
    Signed-off-by: Tair Rzayev <tair.rzayev@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/android/binder.c
+++ drivers/staging/android/binder.c
@@ -1844,7 +1844,6 @@ static void binder_transaction(struct binder_proc *proc,
 	struct binder_buffer_object *last_fixup_obj = NULL;
 	binder_size_t last_fixup_min_off = 0;
 	struct binder_context *context = proc->context;
-	const struct cred *cred = __task_cred(proc->tsk);
 
 	e = binder_transaction_log_add(&binder_transaction_log);
 	e->call_type = reply ? 2 : !!(tr->flags & TF_ONE_WAY);
@@ -1993,7 +1992,7 @@ static void binder_transaction(struct binder_proc *proc,
 		t->from = thread;
 	else
 		t->from = NULL;
-	t->sender_euid = cred->euid;
+	t->sender_euid = task_euid(proc->tsk);
 	t->to_proc = target_proc;
 	t->to_thread = target_thread;
 	t->code = tr->code;
