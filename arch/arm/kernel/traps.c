28eb92c89022f2a0cf540768f0312e377ccd573b
Nikolay Borisov <Nikolay.Borisov@arm.com>
2014-06-03 19:50:09 +0100

    ARM: 8074/1: traps: Make use of the frame_pointer macro
    Use the newly-introduced frame_pointer macro to extract
    the correct FP based on whether we are in THUMB2 mode or not.
    
    Signed-off-by: Nikolay Borisov <Nikolay.Borisov@arm.com>
    Acked-by: Will Deacon <will.deacon@arm.com>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>
    Change-Id: I765294aa02dffbc24975df86d2e66c7726b5b667

--- arch/arm/kernel/traps.c
+++ arch/arm/kernel/traps.c
@@ -31,11 +31,13 @@
 #include <asm/exception.h>
 #include <asm/unistd.h>
 #include <asm/traps.h>
+#include <asm/ptrace.h>
 #include <asm/unwind.h>
 #include <asm/tls.h>
 #include <asm/system_misc.h>
 
 #include <trace/events/exception.h>
+
 
 static const char *handler[]= {
 	"prefetch abort",
@@ -185,7 +187,7 @@ static void dump_backtrace(struct pt_regs *regs, struct task_struct *tsk)
 		tsk = current;
 
 	if (regs) {
-		fp = regs->ARM_fp;
+		fp = frame_pointer(regs);
 		mode = processor_mode(regs);
 	} else if (tsk != current) {
 		fp = thread_saved_fp(tsk);
@@ -680,7 +682,7 @@ asmlinkage int arm_syscall(int no, struct pt_regs *regs)
 		dump_instr("", regs);
 		if (user_mode(regs)) {
 			__show_regs(regs);
-			c_backtrace(regs->ARM_fp, processor_mode(regs));
+			c_backtrace(frame_pointer(regs), processor_mode(regs));
 		}
 	}
 #endif
