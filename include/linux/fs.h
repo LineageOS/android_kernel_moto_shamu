66aaf44c4430c487a1c170cf9fd52271b0d2f2df
Christoph Hellwig <hch@lst.de>
2014-07-13 17:00:37 +0200

    locks: typedef fl_owner_t to void *
    fl_owner_t is a cookie that can store all kinds of different pointers,
    so don't pretends it points to a file structure.
    
    For now just change the typedef, but as a follow on this will allow
    to get rids of lots of casts and eventually the typedef itself.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Jeff Layton <jlayton@primarydata.com>
    Change-Id: I197cdfff4c84aa8fcb7a2f1c7952b54e8f679f8e

--- include/linux/fs.h
+++ include/linux/fs.h
@@ -939,7 +939,7 @@ static inline int file_check_writeable(struct file *filp)
  *
  * Lockd stuffs a "host" pointer into this.
  */
-typedef struct files_struct *fl_owner_t;
+typedef void *fl_owner_t;
 
 struct file_lock_operations {
 	void (*fl_copy_lock)(struct file_lock *, struct file_lock *);
