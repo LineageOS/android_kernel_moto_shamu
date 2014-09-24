d56a84cb87afb3e04939853b14ae08abb915f07b
Peter Zijlstra <peterz@infradead.org>
2014-09-24 10:18:49 +0200

    sched, exit: Deal with nested sleeps
    do_wait() is a big wait loop, but we set TASK_RUNNING too late; we end
    up calling potential sleeps before we reset it.
    
    Not strictly a bug since we're guaranteed to exit the loop and not
    call schedule(); put in annotations to quiet might_sleep().
    
     WARNING: CPU: 0 PID: 1 at ../kernel/sched/core.c:7123 __might_sleep+0x7e/0x90()
     do not call blocking ops when !TASK_RUNNING; state=1 set at [<ffffffff8109a788>] do_wait+0x88/0x270
    
     Call Trace:
      [<ffffffff81694991>] dump_stack+0x4e/0x7a
      [<ffffffff8109877c>] warn_slowpath_common+0x8c/0xc0
      [<ffffffff8109886c>] warn_slowpath_fmt+0x4c/0x50
      [<ffffffff810bca6e>] __might_sleep+0x7e/0x90
      [<ffffffff811a1c15>] might_fault+0x55/0xb0
      [<ffffffff8109a3fb>] wait_consider_task+0x90b/0xc10
      [<ffffffff8109a804>] do_wait+0x104/0x270
      [<ffffffff8109b837>] SyS_wait4+0x77/0x100
      [<ffffffff8169d692>] system_call_fastpath+0x16/0x1b
    
    Change-Id: I6d0a7e2ac676fe95e27068518d1fe69108864fe9
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Cc: tglx@linutronix.de
    Cc: umgwanakikbuti@gmail.com
    Cc: ilya.dryomov@inktank.com
    Cc: Alex Elder <alex.elder@linaro.org>
    Cc: Andrew Morton <akpm@linux-foundation.org>
    Cc: Axel Lin <axel.lin@ingics.com>
    Cc: Daniel Borkmann <dborkman@redhat.com>
    Cc: Dave Jones <davej@redhat.com>
    Cc: Guillaume Morin <guillaume@morinfr.org>
    Cc: Ionut Alexa <ionut.m.alexa@gmail.com>
    Cc: Jason Baron <jbaron@akamai.com>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Michal Hocko <mhocko@suse.cz>
    Cc: Michal Schmidt <mschmidt@redhat.com>
    Cc: Oleg Nesterov <oleg@redhat.com>
    Cc: Paul E. McKenney <paulmck@linux.vnet.ibm.com>
    Cc: Rik van Riel <riel@redhat.com>
    Cc: Rusty Russell <rusty@rustcorp.com.au>
    Cc: Steven Rostedt <rostedt@goodmis.org>
    Link: http://lkml.kernel.org/r/20140924082242.186408915@infradead.org
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- kernel/exit.c
+++ kernel/exit.c
@@ -1001,6 +1001,8 @@ static int wait_task_zombie(struct wait_opts *wo, struct task_struct *p)
 
 		get_task_struct(p);
 		read_unlock(&tasklist_lock);
+		sched_annotate_sleep();
+
 		if ((exit_code & 0x7f) == 0) {
 			why = CLD_EXITED;
 			status = exit_code >> 8;
@@ -1081,6 +1083,7 @@ static int wait_task_zombie(struct wait_opts *wo, struct task_struct *p)
 	 * thread can reap it because we its state == DEAD/TRACE.
 	 */
 	read_unlock(&tasklist_lock);
+	sched_annotate_sleep();
 
 	retval = wo->wo_rusage
 		? getrusage(p, RUSAGE_BOTH, wo->wo_rusage) : 0;
@@ -1212,6 +1215,7 @@ unlock_sig:
 	pid = task_pid_vnr(p);
 	why = ptrace ? CLD_TRAPPED : CLD_STOPPED;
 	read_unlock(&tasklist_lock);
+	sched_annotate_sleep();
 
 	if (unlikely(wo->wo_flags & WNOWAIT))
 		return wait_noreap_copyout(wo, p, pid, uid, why, exit_code);
@@ -1274,6 +1278,7 @@ static int wait_task_continued(struct wait_opts *wo, struct task_struct *p)
 	pid = task_pid_vnr(p);
 	get_task_struct(p);
 	read_unlock(&tasklist_lock);
+	sched_annotate_sleep();
 
 	if (!wo->wo_info) {
 		retval = wo->wo_rusage
