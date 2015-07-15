6143015c7736c4ac9a5368c93a24f31376543ab8
Fred Oh <fred@codeaurora.org>
2015-07-15 09:38:53 -0700

    ASoC: msm: qdsp6v2: fix incorrect packet size calculation
    APR header size is included twice which cause memory out of bounds. So
    remove adding the header size again.
    
    Change-Id: I059aaa8ff77dc32431fe5b52ad27f90df890c979
    Signed-off-by: Fred Oh <fred@codeaurora.org>

--- sound/soc/msm/qdsp6v2/q6adm.c
+++ sound/soc/msm/qdsp6v2/q6adm.c
@@ -986,8 +986,7 @@ static int send_adm_cal_block(int port_id, struct acdb_cal_block *aud_cal,
 
 	adm_params.hdr.hdr_field = APR_HDR_FIELD(APR_MSG_TYPE_SEQ_CMD,
 		APR_HDR_LEN(20), APR_PKT_VER);
-	adm_params.hdr.pkt_size = APR_PKT_SIZE(APR_HDR_SIZE,
-		sizeof(adm_params));
+	adm_params.hdr.pkt_size = sizeof(adm_params);
 	adm_params.hdr.src_svc = APR_SVC_ADM;
 	adm_params.hdr.src_domain = APR_DOMAIN_APPS;
 	adm_params.hdr.src_port = port_id;
