421db4c676712590789cfb5b3d0405794114d9b8
Chaotian Jing <chaotian.jing@mediatek.com>
2016-05-19 16:47:42 +0800

    mmc: mmc: fix switch timeout issue caused by jiffies precision
    [ Upstream commit 987aa5f8059613bf85cbb6f64ffbd34f5cb7a9d1 ]
    
    with CONFIG_HZ=100, the precision of jiffies is 10ms, and the
    generic_cmd6_time of some card is also 10ms. then, may be current
    time is only 5ms, but already timed out caused by jiffies precision.
    
    Change-Id: Ib412dc7464377b4ba0ccf7657cc5a5b6bb68ecde
    Signed-off-by: Chaotian Jing <chaotian.jing@mediatek.com>
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/mmc/core/mmc_ops.c
+++ drivers/mmc/core/mmc_ops.c
@@ -444,7 +444,7 @@ int __mmc_switch(struct mmc_card *card, u8 set, u8 index, u8 value,
 		return 0;
 
 	/* Must check status to be sure of no errors */
-	timeout = jiffies + msecs_to_jiffies(MMC_OPS_TIMEOUT_MS);
+	timeout = jiffies + msecs_to_jiffies(MMC_OPS_TIMEOUT_MS) + 1;
 	do {
 		err = mmc_send_status(card, &status);
 		if (err)
