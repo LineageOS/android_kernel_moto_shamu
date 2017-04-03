d78f71dfca97b93705ba376e2422b049a85d0f6d
Guillaume Nault <g.nault@alphalink.fr>
2017-04-03 12:03:13 +0200

    l2tp: take reference on sessions being dumped [CONTEXT FIX ONLY]
    [ Original commit 43f8c8b058214682ec49a6c9a4a3d6bb1a0d6b84 ]
    
    Change-Id: Ic279420fd349629353b9487544eadaede5930e7e

--- net/l2tp/l2tp_core.h
+++ net/l2tp/l2tp_core.h
@@ -236,7 +236,7 @@ struct l2tp_session *l2tp_session_get(const struct net *net,
 				      struct l2tp_tunnel *tunnel,
 				      u32 session_id, bool do_ref);
 struct l2tp_session *l2tp_session_get_nth(struct l2tp_tunnel *tunnel, int nth,
-						 bool do_ref);
+					  bool do_ref);
 struct l2tp_session *l2tp_session_get_by_ifname(const struct net *net,
 						const char *ifname,
 						bool do_ref);
