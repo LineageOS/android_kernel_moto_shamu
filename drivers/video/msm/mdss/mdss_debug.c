abc9836af181600adb40f1e625d8827bcc4b6f0e
Harsh Sahu <hsahu@codeaurora.org>
2017-05-24 13:37:28 -0700

    msm : mdss: fix unaligned access problem
    debug offset comes from the user and can hold any value which can
    cause unaligned access. This change fixes the unaligned access
    problem on debug offset by properly aligning it.
    
    Change-Id: Ic61c2651986ea6c98cc7d58e27af3e5fe6e42a88
    Signed-off-by: Harsh Sahu <hsahu@codeaurora.org>

--- drivers/video/msm/mdss/mdss_debug.c
+++ drivers/video/msm/mdss/mdss_debug.c
@@ -85,6 +85,9 @@ static ssize_t mdss_debug_base_offset_write(struct file *file,
 
 	buf[count] = 0;	/* end of string */
 
+	if (off % sizeof(u32))
+		return -EINVAL;
+
 	sscanf(buf, "%5x %x", &off, &cnt);
 
 	if (off > dbg->max_offset)
@@ -151,6 +154,9 @@ static ssize_t mdss_debug_base_reg_write(struct file *file,
 	if (cnt < 2)
 		return -EFAULT;
 
+	if (off % sizeof(u32))
+		return -EFAULT;
+
 	if (off >= dbg->max_offset)
 		return -EFAULT;
 
@@ -192,6 +198,9 @@ static ssize_t mdss_debug_base_reg_read(struct file *file,
 			pr_err("not enough memory to hold reg dump\n");
 			return -ENOMEM;
 		}
+
+		if (dbg->off % sizeof(u32))
+			return -EFAULT;
 
 		ptr = dbg->base + dbg->off;
 		tot = 0;
