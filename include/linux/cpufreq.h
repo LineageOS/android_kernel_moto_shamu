2d0ce033d85cc8a29218eb90673fa25594cf4687
Connor O'Brien <connoro@google.com>
2017-10-16 10:41:22 -0700

    ANDROID: cpufreq: Add time_in_state to /proc/uid directories
    Add per-uid files that report the data in binary format rather than
    text, to allow faster reading & parsing by userspace.
    
    Signed-off-by: Connor O'Brien <connoro@google.com>
    Bug: 66953705
    Test: compare values to those reported in /proc/uid_time_in_state
    Change-Id: I9f7ad7146ab7c589336706e6fa19c20c4c97aec1

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -13,6 +13,7 @@
 
 #include <linux/pid_namespace.h>
 #include <linux/cpumask.h>
+#include <linux/cputime.h>
 #include <linux/completion.h>
 #include <linux/kobject.h>
 #include <linux/notifier.h>
@@ -480,5 +481,6 @@ void cpufreq_task_stats_init(struct task_struct *p);
 void cpufreq_task_stats_remove_uids(uid_t uid_start, uid_t uid_end);
 int  proc_time_in_state_show(struct seq_file *m, struct pid_namespace *ns,
 	struct pid *pid, struct task_struct *p);
+int single_uid_time_in_state_open(struct inode *inode, struct file *file);
 
 #endif /* _LINUX_CPUFREQ_H */
