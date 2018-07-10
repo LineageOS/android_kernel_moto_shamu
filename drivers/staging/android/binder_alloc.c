660b936991ccf6158806763e42ffaf8510fb1109
dev-harsh1998 <harshitjain6751@gmail.com>
2018-07-10 20:25:34 +0530

    android: binder: replace ACCESS_ONCE with conventional 3.18 specifiers
    * Thanks a lot to @remilia15 for pointing this out.
    * Since we already picked the macro conversion commits cb31ff7c818458d25d6598c65bb2a25134a9ef55 325387189a1605d21e70faeb4475827d4ca4e253 there is now no need to use legacy specifier
    
    Co-authored-by: Remilia Scarlet <remilia.1505@gmail.com>
    Signed-off-by: Remilia Scarlet <remilia.1505@gmail.com>
    Signed-off-by: dev-harsh1998 <harshitjain6751@gmail.com>
    Change-Id: Ibc7ef9994b08ea47037e34d6b98f30663a1ebb38

--- drivers/staging/android/binder_alloc.c
+++ drivers/staging/android/binder_alloc.c
@@ -812,8 +812,8 @@ int binder_alloc_get_allocated_count(struct binder_alloc *alloc)
  */
 void binder_alloc_vma_close(struct binder_alloc *alloc)
 {
-	ACCESS_ONCE(alloc->vma) = NULL;
-	ACCESS_ONCE(alloc->vma_vm_mm) = NULL;
+	WRITE_ONCE(alloc->vma, NULL);
+	WRITE_ONCE(alloc->vma_vm_mm, NULL);
 }
 
 /**
