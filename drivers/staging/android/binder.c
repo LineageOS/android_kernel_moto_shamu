2ab4b94f63c843bf3b7f49cb0e6f8cee0cee3bb9
dev-harsh1998 <harshitjain6751@gmail.com>
2018-07-10 20:25:34 +0530

    android: binder: replace ACCESS_ONCE with conventional 3.18 specifiers
    * Thanks a lot to @remilia15 for pointing this out.
    * Since we already picked the macro conversion commits cb31ff7c818458d25d6598c65bb2a25134a9ef55 325387189a1605d21e70faeb4475827d4ca4e253 there is now no need to use legacy specifier
    
    Co-authored-by: Remilia Scarlet <remilia.1505@gmail.com>
    Signed-off-by: Remilia Scarlet <remilia.1505@gmail.com>
    Signed-off-by: dev-harsh1998 <harshitjain6751@gmail.com>
    Change-Id: Ibc7ef9994b08ea47037e34d6b98f30663a1ebb38

--- drivers/staging/android/binder.c
+++ drivers/staging/android/binder.c
@@ -253,7 +253,7 @@ static struct binder_transaction_log_entry *binder_transaction_log_add(
 	if (cur >= ARRAY_SIZE(log->entry))
 		log->full = 1;
 	e = &log->entry[cur % ARRAY_SIZE(log->entry)];
-	ACCESS_ONCE(e->debug_id_done) = 0;
+	WRITE_ONCE(e->debug_id_done, 0);
 	/*
 	 * write-barrier to synchronize access to e->debug_id_done.
 	 * We make sure the initialized 0 value is seen before
@@ -3246,7 +3246,7 @@ static void binder_transaction(struct binder_proc *proc,
 	 * of log entry
 	 */
 	smp_wmb();
-	ACCESS_ONCE(e->debug_id_done) = t_debug_id;
+	WRITE_ONCE(e->debug_id_done, t_debug_id);
 	return;
 
 err_dead_proc_or_thread:
@@ -3301,8 +3301,8 @@ err_no_context_mgr_node:
 		 * of log entry
 		 */
 		smp_wmb();
-		ACCESS_ONCE(e->debug_id_done) = t_debug_id;
-		ACCESS_ONCE(fe->debug_id_done) = t_debug_id;
+		WRITE_ONCE(e->debug_id_done, t_debug_id);
+		WRITE_ONCE(fe->debug_id_done, t_debug_id);
 	}
 
 	BUG_ON(thread->return_error.cmd != BR_OK);
@@ -5526,7 +5526,7 @@ static int binder_proc_show(struct seq_file *m, void *unused)
 static void print_binder_transaction_log_entry(struct seq_file *m,
 					struct binder_transaction_log_entry *e)
 {
-	int debug_id = ACCESS_ONCE(e->debug_id_done);
+	int debug_id = READ_ONCE(e->debug_id_done);
 	/*
 	 * read barrier to guarantee debug_id_done read before
 	 * we print the log values
@@ -5545,7 +5545,7 @@ static void print_binder_transaction_log_entry(struct seq_file *m,
 	 * done printing the fields of the entry
 	 */
 	smp_rmb();
-	seq_printf(m, debug_id && debug_id == ACCESS_ONCE(e->debug_id_done) ?
+	seq_printf(m, debug_id && debug_id == READ_ONCE(e->debug_id_done) ?
 			"\n" : " (incomplete)\n");
 }
 
