5d37367b77224a4ec78e1774535f3e6ad866152d
Wang Hai <wanghai26@huawei.com>
2019-03-20 14:25:05 -0400

    net-sysfs: Fix memory leak in netdev_register_kobject
    commit 6b70fc94afd165342876e53fc4b2f7d085009945 upstream.
    
    When registering struct net_device, it will call
    	register_netdevice ->
    		netdev_register_kobject ->
    			device_initialize(dev);
    			dev_set_name(dev, "%s", ndev->name)
    			device_add(dev)
    			register_queue_kobjects(ndev)
    
    In netdev_register_kobject(), if device_add(dev) or
    register_queue_kobjects(ndev) failed. Register_netdevice()
    will return error, causing netdev_freemem(ndev) to be
    called to free net_device, however put_device(&dev->dev)->..->
    kobject_cleanup() won't be called, resulting in a memory leak.
    
    syzkaller report this:
    BUG: memory leak
    unreferenced object 0xffff8881f4fad168 (size 8):
    comm "syz-executor.0", pid 3575, jiffies 4294778002 (age 20.134s)
    hex dump (first 8 bytes):
      77 70 61 6e 30 00 ff ff                          wpan0...
    backtrace:
      [<000000006d2d91d7>] kstrdup_const+0x3d/0x50 mm/util.c:73
      [<00000000ba9ff953>] kvasprintf_const+0x112/0x170 lib/kasprintf.c:48
      [<000000005555ec09>] kobject_set_name_vargs+0x55/0x130 lib/kobject.c:281
      [<0000000098d28ec3>] dev_set_name+0xbb/0xf0 drivers/base/core.c:1915
      [<00000000b7553017>] netdev_register_kobject+0xc0/0x410 net/core/net-sysfs.c:1727
      [<00000000c826a797>] register_netdevice+0xa51/0xeb0 net/core/dev.c:8711
      [<00000000857bfcfd>] cfg802154_update_iface_num.isra.2+0x13/0x90 [ieee802154]
      [<000000003126e453>] ieee802154_llsec_fill_key_id+0x1d5/0x570 [ieee802154]
      [<00000000e4b3df51>] 0xffffffffc1500e0e
      [<00000000b4319776>] platform_drv_probe+0xc6/0x180 drivers/base/platform.c:614
      [<0000000037669347>] really_probe+0x491/0x7c0 drivers/base/dd.c:509
      [<000000008fed8862>] driver_probe_device+0xdc/0x240 drivers/base/dd.c:671
      [<00000000baf52041>] device_driver_attach+0xf2/0x130 drivers/base/dd.c:945
      [<00000000c7cc8dec>] __driver_attach+0x10e/0x210 drivers/base/dd.c:1022
      [<0000000057a757c2>] bus_for_each_dev+0x154/0x1e0 drivers/base/bus.c:304
      [<000000005f5ae04b>] bus_add_driver+0x427/0x5e0 drivers/base/bus.c:645
    
    Change-Id: I7f0b3c1f1747a2b387a697f3a2f3e6ac80d34e08
    Reported-by: Hulk Robot <hulkci@huawei.com>
    Fixes: 1fa5ae857bb1 ("driver core: get rid of struct device's bus_id string array")
    Signed-off-by: Wang Hai <wanghai26@huawei.com>
    Reviewed-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    Reviewed-by: Stephen Hemminger <stephen@networkplumber.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    [bwh: Backported to 3.16: adjust context]
    Signed-off-by: Ben Hutchings <ben@decadent.org.uk>
    (cherry picked from commit be558dfef222847c7c01f4b4881dccc8f48a0905)

--- net/core/net-sysfs.c
+++ net/core/net-sysfs.c
@@ -1286,16 +1286,20 @@ int netdev_register_kobject(struct net_device *net)
 
 	error = device_add(dev);
 	if (error)
-		return error;
+		goto error_put_device;
 
 	error = register_queue_kobjects(net);
-	if (error) {
-		device_del(dev);
-		return error;
-	}
+	if (error)
+		goto error_device_del;
 
 	pm_runtime_set_memalloc_noio(dev, true);
 
+	return 0;
+
+error_device_del:
+	device_del(dev);
+error_put_device:
+	put_device(dev);
 	return error;
 }
 
