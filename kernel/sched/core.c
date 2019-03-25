fda6b0d97ab76e70eb7f722c1031cbdfe6dffc98
Kim Le <elektroschmock78@googlemail.com>
2019-03-25 10:12:59 +0100

    Revert "kernel: initialize and free cpufreq stats properly"
    This reverts commit d69ff86853f524c93095972104fd57f79289da13.
    
    For info see discussion @ https://review.lineageos.org/244670
    
    Change-Id: I4cc34e3e493702583487f24b83f95f6ad9eccf1b

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -1897,10 +1897,6 @@ static void __sched_fork(struct task_struct *p)
 	memset(&p->se.statistics, 0, sizeof(p->se.statistics));
 #endif
 
-#ifdef CONFIG_CPU_FREQ_STAT
-	cpufreq_task_stats_init(p);
-#endif
-
 	INIT_LIST_HEAD(&p->rt.run_list);
 
 #ifdef CONFIG_PREEMPT_NOTIFIERS
@@ -1949,11 +1945,11 @@ void sched_fork(struct task_struct *p)
 	unsigned long flags;
 	int cpu = get_cpu();
 
-	__sched_fork(p);
-
 #ifdef CONFIG_CPU_FREQ_STAT
-	cpufreq_task_stats_alloc(p);
+	cpufreq_task_stats_init(p);
 #endif
+
+	__sched_fork(p);
 
 	/*
 	 * We mark the process as running here. This guarantees that
