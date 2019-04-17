721b1ba91f6ed1a423f6e2b0fa51101cff9cc2c8
Elektroschmock <elektroschmock78@googlemail.com>
2019-04-17 21:33:49 +0200

    Revert "mm/oom_kill: remove the wrong fatal_signal_pending() check in oom_kill_process()"
    This reverts commit 50a156825a818377826f2b154ce3a2e5daec045c.

--- mm/oom_kill.c
+++ mm/oom_kill.c
@@ -585,7 +585,11 @@ void oom_kill_process(struct task_struct *p, gfp_t gfp_mask, int order,
 		    !(p->flags & PF_KTHREAD)) {
 			if (p->signal->oom_score_adj == OOM_SCORE_ADJ_MIN)
 				continue;
+			if (fatal_signal_pending(p))
+				continue;
 
+			pr_info("Kill process %d (%s) sharing same memory\n",
+				task_pid_nr(p), p->comm);
 			do_send_sig_info(SIGKILL, SEND_SIG_FORCED, p, true);
 		}
 	rcu_read_unlock();
