5ce2ed8dd125b49912905bbe9fe98db6fd587ac2
elektroschmock <elektroschmock78@googlemail.com>
2019-05-22 08:18:22 +0200

    msm: sensor: Downgrade log from error to info
    Change-Id: I525f62a15300a7d60f136793295b8e9a83ce3d60

--- drivers/media/platform/msm/camera_v2/sensor/msm_sensor_driver.c
+++ drivers/media/platform/msm/camera_v2/sensor/msm_sensor_driver.c
@@ -816,7 +816,7 @@ int32_t msm_sensor_driver_probe(void *setting)
 		goto FREE_CAMERA_INFO;
 	}
 
-	pr_err("%s probe succeeded", slave_info->sensor_name);
+	pr_info("%s probe succeeded\n", slave_info->sensor_name);
 
 	/* Save sensor info*/
 	s_ctrl->sensordata->cam_slave_info = slave_info;
