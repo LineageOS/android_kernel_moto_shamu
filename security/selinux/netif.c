919d12ee7461fe16607ef274f87f1c23c93525b4
elektroschmock <elektroschmock78@googlemail.com>
2019-05-23 07:43:11 +0200

    selinux: netif: Fix missing braces
    Change-Id: I63b3635d629a5935538366041e0af2bd2477bbc5

--- security/selinux/netif.c
+++ security/selinux/netif.c
@@ -55,7 +55,7 @@ static struct list_head sel_netif_hash[SEL_NETIF_HASH_SIZE];
  */
 static inline u32 sel_netif_hashfn(const struct net *ns, int ifindex)
 {
-	return ((uintptr_t)ns + ifindex) & (SEL_NETIF_HASH_SIZE - 1);
+	return (((uintptr_t)ns + ifindex) & (SEL_NETIF_HASH_SIZE - 1));
 }
 
 /**
