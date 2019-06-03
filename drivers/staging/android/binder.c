9d1c75426060ab1fd21beed39d8b498dd3d8a4f6
elektroschmock <elektroschmock78@googlemail.com>
2019-06-03 15:57:40 +0200

    Revert "android: binder: move preempt disable after mmput"
    This reverts commit 2087affd75f23d34b332ebd4d2da151b0951bf54.
    
    Revert to apply the upstream solution
    
    Change-Id: Iac441b5beb340ec0f7c102e3b2ee178bdee351e6

--- drivers/staging/android/binder.c
+++ drivers/staging/android/binder.c
@@ -702,13 +702,11 @@ static int binder_update_page_range(struct binder_proc *proc, int allocate,
 		}
 		/* vm_insert_page does not seem to increment the refcount */
 	}
+	preempt_disable();
 	if (mm) {
 		up_write(&mm->mmap_sem);
 		mmput(mm);
 	}
-
-	preempt_disable();
-
 	return 0;
 
 free_range:
@@ -727,13 +725,11 @@ err_alloc_page_failed:
 		;
 	}
 err_no_vma:
+	preempt_disable();
 	if (mm) {
 		up_write(&mm->mmap_sem);
 		mmput(mm);
 	}
-
-	preempt_disable();
-
 	return -ENOMEM;
 }
 
