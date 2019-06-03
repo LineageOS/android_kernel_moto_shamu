134f5704b44d03c459eca3173735c13837ee62e3
elektroschmock <elektroschmock78@googlemail.com>
2019-06-03 12:31:39 +0200

    Revert "ANDROID: binder: fix OOB write in __binder_update_page_range"
    This reverts commit 777e744063a7db243a5a618a6c2a1f3d55056d82.
    
    Revert to apply the upstream solution
    
    Change-Id: I4399e4bd118d41cab505bdac7df3480fca5ecc5e

--- drivers/staging/android/binder.c
+++ drivers/staging/android/binder.c
@@ -3487,11 +3487,6 @@ static int binder_mmap(struct file *filp, struct vm_area_struct *vma)
 		}
 	}
 #endif
-	if (vma->vm_end - vma->vm_start < BINDER_MIN_ALLOC) {
-		ret = -EINVAL;
-		failure_string = "VMA size < BINDER_MIN_ALLOC";
-		goto err_vma_too_small;
-	}
 	proc->pages = kzalloc(sizeof(proc->pages[0]) * ((vma->vm_end - vma->vm_start) / PAGE_SIZE), GFP_KERNEL);
 	if (proc->pages == NULL) {
 		ret = -ENOMEM;
@@ -3542,7 +3537,6 @@ err_alloc_buf_struct_failed:
 	kfree(proc->pages);
 	proc->pages = NULL;
 err_alloc_pages_failed:
-	err_vma_too_small:
 	mutex_lock(&binder_mmap_lock);
 	vfree(proc->buffer);
 	proc->buffer = NULL;
