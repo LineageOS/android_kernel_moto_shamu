17c2d53b366d0f9a4afafb7ebc677f8a0a020f27
Soumya Managoli <smanag@codeaurora.org>
2019-10-03 15:20:42 +0530

    asoc: msm-pcm: Added lock in controls _put() and _get() callback
    There is race condition around private data used in put() and get()
    of few mixer ctls with close() callback.Add global mutex lock and
    code changes to protect such critical section by accessing such lock.
    
    Change-Id: I276c2a234cfcbef88b4272b945e5c3f121e8eb32
    Signed-off-by: Soumya Managoli <smanag@codeaurora.org>

--- sound/soc/msm/qdsp6v2/msm-pcm-q6-v2.h
+++ sound/soc/msm/qdsp6v2/msm-pcm-q6-v2.h
@@ -1,7 +1,7 @@
 /*
  * Copyright (C) 2008 Google, Inc.
  * Copyright (C) 2008 HTC Corporation
- * Copyright (c) 2012-2013 The Linux Foundation. All rights reserved.
+ * Copyright (c) 2012-2015, 2019 The Linux Foundation. All rights reserved.
  *
  * This software is licensed under the terms of the GNU General Public
  * License version 2, as published by the Free Software Foundation, and
@@ -106,6 +106,7 @@ struct output_meta_data_st {
 
 struct msm_plat_data {
 	int perf_mode;
+	struct mutex lock;
 };
 
 #endif /*_MSM_PCM_H*/
